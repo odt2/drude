dde51f519a1046da12e869aafd63b3accad08845
odt2 <odt2@pitt.edu>
2014-07-05 16:28:09 -0400

    Initial commit from local git

---
+++ drude/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  drude
+//
+//  Created by Tuguldur Odbadrakh on 7/5/14.
+//  Copyright (c) 2014 Tuguldur Odbadrakh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
